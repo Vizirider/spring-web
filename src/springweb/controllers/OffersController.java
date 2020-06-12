2196bb1f44285267b76bdaf9a2cdf4b5c26fe705
Vizi Robert <vizirider@gmail.com>
2020-06-12 16:40:48 +0200

    web mvc
    mvc with dao control and service

--- src/springweb/controllers/OffersController.java
+++ src/springweb/controllers/OffersController.java
@@ -1,12 +1,50 @@
 package springweb.controllers;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import springweb.dao.Offer;
+import springweb.service.OffersService;
 
 @Controller
 public class OffersController {
 	
-	@RequestMapping("/")
-	public String showHome() {
-		return "home";
+	private OffersService offersService;
+
+	@Autowired
+	public void setOffersService(OffersService offersService) {
+		this.offersService = offersService;
 	}
+	
+	@RequestMapping(value="/test", method = RequestMethod.GET)
+	public String showTest(Model model, @RequestParam("id") String id) {
+		 
+		System.out.println("Id is: " + id );
+		return "home";
+	} 
+	
+	@RequestMapping("/offers")
+	public String showOffers(Model model) {
+		 
+		List<Offer> offers = offersService.getCurrent();
+		
+		model.addAttribute("offers", offers);
+		
+		return "offers";
+	} 
+
+	@RequestMapping("/createoffer")
+	public String createOffers(Model model) {
+		
+		return "createoffer";
+	} 
 }
---
+++ src/springweb/service/OffersService.java
@@ -0,0 +1,24 @@
+package springweb.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import springweb.dao.Offer;
+import springweb.dao.OffersDAO;
+
+@Service("offersService")
+public class OffersService {
+	
+	private OffersDAO offersDao;
+	
+	@Autowired
+	public void setOffersDao(OffersDAO offersDao) {
+		this.offersDao = offersDao;
+	}
+
+	public List<Offer> getCurrent(){
+		return offersDao.getOffers();
+	}
+}
