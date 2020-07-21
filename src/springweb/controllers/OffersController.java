476c22a787d67138ee4c62401e3f06467d1847fb
Vizi Robert <vizirider@gmail.com>
2020-07-21 11:13:52 +0200

    create offer and validator
    offer create dao service and jsp with validator

--- src/springweb/controllers/OffersController.java
+++ src/springweb/controllers/OffersController.java
@@ -3,10 +3,12 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,32 +21,45 @@ import springweb.service.OffersService;
 public class OffersController {
 	
 	private OffersService offersService;
-
+	
 	@Autowired
 	public void setOffersService(OffersService offersService) {
 		this.offersService = offersService;
 	}
 	
-	@RequestMapping(value="/test", method = RequestMethod.GET)
+	@RequestMapping(value="/test", method=RequestMethod.GET)
 	public String showTest(Model model, @RequestParam("id") String id) {
-		 
-		System.out.println("Id is: " + id );
+		System.out.println("Id is: " + id);
 		return "home";
-	} 
-	
+	}
+
 	@RequestMapping("/offers")
 	public String showOffers(Model model) {
-		 
+		
 		List<Offer> offers = offersService.getCurrent();
 		
 		model.addAttribute("offers", offers);
 		
 		return "offers";
-	} 
-
+	}
+	
 	@RequestMapping("/createoffer")
-	public String createOffers(Model model) {
+	public String createOffer(Model model) {
+	
+		model.addAttribute("offer", new Offer());
 		
 		return "createoffer";
-	} 
+	}
+	
+	@RequestMapping(value="/docreate", method=RequestMethod.POST)
+	public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "createoffer";
+		}
+		
+		offersService.create(offer);
+		
+		return "offercreated";
+	}
 }
